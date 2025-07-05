a91c4a4415a75df32fe65003900ddfba4e0460c3
ManuelGeorges <manuel.georges2009@email.com>
2025-07-05 17:07:33 +0300

    solved the security danger on data

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCbMBLE3r_qmVwaruRWm3tu9um5wqgS9DY",
-  authDomain: "keraza-shark.firebaseapp.com",
-  projectId: "keraza-shark",
-  storageBucket: "keraza-shark.firebasestorage.app",
-  messagingSenderId: "126837559653",
-  appId: "1:126837559653:web:39199642dd1dcb31b0918c",
-  measurementId: "G-ZCNS652NET"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
