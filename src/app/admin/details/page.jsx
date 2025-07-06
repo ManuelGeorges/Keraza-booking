437042a43914db248f89606a51e05738cc3ff8e4
ManuelGeorges <manuel.georges2009@email.com>
2025-07-06 14:42:17 +0300

    added all competitions names translation to Arabic

--- src/app/admin/details/page.jsx
+++ src/app/admin/details/page.jsx
@@ -20,20 +20,129 @@ const COLORS = [
 ];
 
 const competitionNamesInArabic = {
-  // نفس الداتا اللي انت كاتبها، متشالة بالكامل ومتسيبتش حاجة.
+  // مسابقات الكرازة والكنسية
+  "karaza": "مسابقة الكرازة",
+  "alhan": "مسابقة الألحان",
+  "research": "مسابقة البحث",
+  "service": "مسابقة الخدمة",
+  "holy_bible": "مسابقة الكتاب المقدس",
+  "creative": "مسابقة الابتكار",
+  "talents": "مسابقة المواهب",
+  "sports": "المسابقات الرياضية",
+
+  // كرة القدم
+  "football_boys_grade0": "كرة القدم - بنين - حضانة - جماعي",
+  "football_boys_grade12": "كرة القدم - بنين - أولى وثانية ابتدائي - جماعي",
+  "football_boys_grade34": "كرة القدم - بنين - ثالثة ورابعة ابتدائي - جماعي",
+  "football_boys_grade56": "كرة القدم - بنين - خامسة وسادسة ابتدائي - جماعي",
+  "football_girls_grade0": "كرة القدم - بنات - حضانة - جماعي",
+  "football_girls_grade12": "كرة القدم - بنات - أولى وثانية ابتدائي - جماعي",
+  "football_girls_grade34": "كرة القدم - بنات - ثالثة ورابعة ابتدائي - جماعي",
+  "football_girls_grade56": "كرة القدم - بنات - خامسة وسادسة ابتدائي - جماعي",
+
+  // الكرة الطائرة
+  "volleyball_boys_grade0": "الكرة الطائرة - بنين - حضانة - جماعي",
+  "volleyball_boys_grade12": "الكرة الطائرة - بنين - أولى وثانية ابتدائي - جماعي",
+  "volleyball_boys_grade34": "الكرة الطائرة - بنين - ثالثة ورابعة ابتدائي - جماعي",
+  "volleyball_boys_grade56": "الكرة الطائرة - بنين - خامسة وسادسة ابتدائي - جماعي",
+  "volleyball_girls_grade0": "الكرة الطائرة - بنات - حضانة - جماعي",
+  "volleyball_girls_grade12": "الكرة الطائرة - بنات - أولى وثانية ابتدائي - جماعي",
+  "volleyball_girls_grade34": "الكرة الطائرة - بنات - ثالثة ورابعة ابتدائي - جماعي",
+  "volleyball_girls_grade56": "الكرة الطائرة - بنات - خامسة وسادسة ابتدائي - جماعي",
+
+  // تنس الطاولة
+  "table_tennis_boys_individual_grade0": "تنس الطاولة - بنين - فردي - حضانة",
+  "table_tennis_boys_individual_grade12": "تنس الطاولة - بنين - فردي - أولى وثانية ابتدائي",
+  "table_tennis_boys_individual_grade34": "تنس الطاولة - بنين - فردي - ثالثة ورابعة ابتدائي",
+  "table_tennis_boys_individual_grade56": "تنس الطاولة - بنين - فردي - خامسة وسادسة ابتدائي",
+  "table_tennis_boys_group_grade0": "تنس الطاولة - بنين - جماعي - حضانة",
+  "table_tennis_boys_group_grade12": "تنس الطاولة - بنين - جماعي - أولى وثانية ابتدائي",
+  "table_tennis_boys_group_grade34": "تنس الطاولة - بنين - جماعي - ثالثة ورابعة ابتدائي",
+  "table_tennis_boys_group_grade56": "تنس الطاولة - بنين - جماعي - خامسة وسادسة ابتدائي",
+  "table_tennis_girls_individual_grade0": "تنس الطاولة - بنات - فردي - حضانة",
+  "table_tennis_girls_individual_grade12": "تنس الطاولة - بنات - فردي - أولى وثانية ابتدائي",
+  "table_tennis_girls_individual_grade34": "تنس الطاولة - بنات - فردي - ثالثة ورابعة ابتدائي",
+  "table_tennis_girls_individual_grade56": "تنس الطاولة - بنات - فردي - خامسة وسادسة ابتدائي",
+  "table_tennis_girls_group_grade0": "تنس الطاولة - بنات - جماعي - حضانة",
+  "table_tennis_girls_group_grade12": "تنس الطاولة - بنات - جماعي - أولى وثانية ابتدائي",
+  "table_tennis_girls_group_grade34": "تنس الطاولة - بنات - جماعي - ثالثة ورابعة ابتدائي",
+  "table_tennis_girls_group_grade56": "تنس الطاولة - بنات - جماعي - خامسة وسادسة ابتدائي",
+
+  // الشطرنج
+  "chess_boys_individual_grade0": "الشطرنج - بنين - فردي - حضانة",
+  "chess_boys_individual_grade12": "الشطرنج - بنين - فردي - أولى وثانية ابتدائي",
+  "chess_boys_individual_grade34": "الشطرنج - بنين - فردي - ثالثة ورابعة ابتدائي",
+  "chess_boys_individual_grade56": "الشطرنج - بنين - فردي - خامسة وسادسة ابتدائي",
+  "chess_boys_group_grade0": "الشطرنج - بنين - جماعي - حضانة",
+  "chess_boys_group_grade12": "الشطرنج - بنين - جماعي - أولى وثانية ابتدائي",
+  "chess_boys_group_grade34": "الشطرنج - بنين - جماعي - ثالثة ورابعة ابتدائي",
+  "chess_boys_group_grade56": "الشطرنج - بنين - جماعي - خامسة وسادسة ابتدائي",
+  "chess_girls_individual_grade0": "الشطرنج - بنات - فردي - حضانة",
+  "chess_girls_individual_grade12": "الشطرنج - بنات - فردي - أولى وثانية ابتدائي",
+  "chess_girls_individual_grade34": "الشطرنج - بنات - فردي - ثالثة ورابعة ابتدائي",
+  "chess_girls_individual_grade56": "الشطرنج - بنات - فردي - خامسة وسادسة ابتدائي",
+  "chess_girls_group_grade0": "الشطرنج - بنات - جماعي - حضانة",
+  "chess_girls_group_grade12": "الشطرنج - بنات - جماعي - أولى وثانية ابتدائي",
+  "chess_girls_group_grade34": "الشطرنج - بنات - جماعي - ثالثة ورابعة ابتدائي",
+  "chess_girls_group_grade56": "الشطرنج - بنات - جماعي - خامسة وسادسة ابتدائي",
+
+  // الجري
+  "running_boys_grade0": "جري - بنين - فردي - حضانة",
+  "running_boys_grade12": "جري - بنين - فردي - أولى وثانية ابتدائي",
+  "running_boys_grade34": "جري - بنين - فردي - ثالثة ورابعة ابتدائي",
+  "running_boys_grade56": "جري - بنين - فردي - خامسة وسادسة ابتدائي",
+  "running_girls_grade0": "جري - بنات - فردي - حضانة",
+  "running_girls_grade12": "جري - بنات - فردي - أولى وثانية ابتدائي",
+  "running_girls_grade34": "جري - بنات - فردي - ثالثة ورابعة ابتدائي",
+  "running_girls_grade56": "جري - بنات - فردي - خامسة وسادسة ابتدائي",
+
+  // كونكت فور
+  "connect4_boys_grade0": "كونكت فور - بنين - فردي - حضانة",
+  "connect4_boys_grade12": "كونكت فور - بنين - فردي - أولى وثانية ابتدائي",
+  "connect4_boys_grade34": "كونكت فور - بنين - فردي - ثالثة ورابعة ابتدائي",
+  "connect4_boys_grade56": "كونكت فور - بنين - فردي - خامسة وسادسة ابتدائي",
+  "connect4_girls_grade0": "كونكت فور - بنات - فردي - حضانة",
+  "connect4_girls_grade12": "كونكت فور - بنات - فردي - أولى وثانية ابتدائي",
+  "connect4_girls_grade34": "كونكت فور - بنات - فردي - ثالثة ورابعة ابتدائي",
+  "connect4_girls_grade56": "كونكت فور - بنات - فردي - خامسة وسادسة ابتدائي",
+   // المهرجان
   "festival_subscription": "إشتراك حجز المهرجان للكنيسة (إلزامى)",
-  "rouhi_alex_kindergarten_1": "مرحلة حضانة - الفريق الأول",
-  "rouhi_alex_grade1_2_1": "مرحلة أولى وثانية ابتدائي - الفريق الأول",
-  "rouhi_alex_grade3_4_1": "مرحلة ثالثة ورابعة ابتدائي - الفريق الأول",
-  "rouhi_alex_grade5_6_1": "مرحلة خامسة وسادسة ابتدائي - الفريق الأول",
-  "rouhi_alex_kindergarten_extra": "مرحلة حضانة - الفريق الإضافي",
-  "rouhi_alex_grade1_2_extra": "مرحلة أولى وثانية ابتدائي - الفريق الإضافي",
-  "rouhi_alex_grade3_4_extra": "مرحلة ثالثة ورابعة ابتدائي - الفريق الإضافي",
-  "rouhi_alex_grade5_6_extra": "مرحلة خامسة وسادسة ابتدائي - الفريق الإضافي",
-  "melodies_level1": "المستوى الأول - فريق",
-  "melodies_level2": "المستوى الثاني - فريق",
-  "melodies_talented_individual": "مستوى الموهوبين - فردي",
-  "melodies_talented_group": "مستوى الموهوبين - جماعي",
+
+  // الروحي الجماعي - اسكندرية
+  "rouhi_alex_kindergarten_1": "روحي مرحلة حضانة - الفريق الأول",
+  "rouhi_alex_grade1_2_1": " روحي مرحلة أولى وثانية ابتدائي - الفريق الأول",
+  "rouhi_alex_grade3_4_1": "روحي مرحلة ثالثة ورابعة ابتدائي - الفريق الأول",
+  "rouhi_alex_grade5_6_1": "روحي مرحلة خامسة وسادسة ابتدائي - الفريق الأول",
+  "rouhi_alex_kindergarten_extra": "روحي مرحلة حضانة - الفريق الإضافي",
+  "rouhi_alex_grade1_2_extra": "روحي مرحلة أولى وثانية ابتدائي - الفريق الإضافي",
+  "rouhi_alex_grade3_4_extra": "روحي مرحلة ثالثة ورابعة ابتدائي - الفريق الإضافي",
+  "rouhi_alex_grade5_6_extra": "روحي مرحلة خامسة وسادسة ابتدائي - الفريق الإضافي",
+
+  // اللغة القبطية - اسكندرية
+  "coptic_alex_kindergarten": "مرحلة حضانة - قبطى",
+  "coptic_alex_grade1_2": "مرحلة أولى وثانية ابتدائي - قبطى",
+  "coptic_alex_grade3_4": "مرحلة ثالثة ورابعة ابتدائي - قبطى",
+  "coptic_alex_grade5_6": "مرحلة خامسة وسادسة ابتدائي - قبطى",
+
+  // الألحان والتسبحة
+  "melodies_level1_Hadana": "المستوى الأول - فريق حضانة - ألحان",
+  "melodies_level2_Hadana": "المستوى الثاني - فريق حضانة - ألحان",
+  "melodies_level1_grades12": "المستوى الأول - فريق أولى وثانية - ألحان",
+  "melodies_level2_grades12": "المستوى الثاني - فريق أولى وثانية - ألحان",
+  "melodies_level1_grades34": "المستوى الأول - فريق ثالثة ورابعة - ألحان",
+  "melodies_level2_grades34": "المستوى الثاني - فريق ثالثة ورابعة - ألحان",
+  "melodies_level1_grades56": "المستوى الأول - فريق خامسة وسادسة - ألحان",
+  "melodies_level2_grades56": "المستوى الثاني - فريق خامسة وسادسة - ألحان",
+
+  "melodies_talented_individual_Hadana": "موهوبين حضانة - فردي - ألحان",
+  "melodies_talented_group_Hadana": "موهوبين حضانة - جماعي - ألحان",
+  "melodies_talented_individual_grades12": "موهوبين أولى وثانية - فردي - ألحان",
+  "melodies_talented_group_grades12": "موهوبين أولى وثانية - جماعي - ألحان",
+  "melodies_talented_individual_grades34": "موهوبين ثالثة ورابعة - فردي - ألحان",
+  "melodies_talented_group_grades34": "موهوبين ثالثة ورابعة - جماعي - ألحان",
+  "melodies_talented_individual_grades56": "موهوبين خامسة وسادسة - فردي - ألحان",
+  "melodies_talented_group_grades56": "موهوبين خامسة وسادسة - جماعي - ألحان",
+ // الأنشطة الكنسية
   "church_activities_big_theatre": "المسرح الكبير - فريق",
   "church_activities_chorus": "الكورال - فريق",
   "church_activities_cantata": "الكنتاتا - فريق",
@@ -43,23 +152,33 @@ const competitionNamesInArabic = {
   "church_activities_music_individual": "مسابقة العزف - فردي",
   "church_activities_solo_team": "مسابقة الصولو - جماعي",
   "church_activities_music_team": "مسابقة العزف - جماعي",
+
+  // مسابقة البحوث
   "research_theoretical": "البحث النظري - فردي",
   "research_cultural": "البحث الثقافي - فردي",
+
+  // المسابقة الثقافية
   "cultural_magazine_paper": "إعداد مجلة ورقية - جماعي",
   "cultural_magazine_wall": "إعداد مجلة حائط - جماعي",
   "cultural_field_visits": "الزيارات الميدانية - جماعي",
-  "electronic_level1_individual": " الإلكترونيات المستوى الأول - فردي ",
-  "electronic_level2_individual": "الإلكترونيات المستوى الثاني - فردي",
-  "electronic_level1_group": " الإلكترونيات المستوى الأول - جماعي",
-  "electronic_level2_group": " الإلكترونيات المستوى الثاني - جماعي",
-  "arts_kindergarten_individual": " الفنون التشكيلة مرحلة حضانة - فردي",
-  "arts_grade1_2_individual": "فنون التشكيلة مرحلة أولى وثانية ابتدائي - فردي",
-  "arts_grade3_4_individual": "فنون التشكيلة مرحلة ثالثة ورابعة ابتدائي - فردي",
-  "arts_grade5_6_individual": "فنون التشكيلة مرحلة خامسة وسادسة ابتدائي - فردي",
-  "arts_kindergarten_group": "فنون التشكيلة مرحلة حضانة - جماعي",
-  "arts_grade1_2_group": "فنون التشكيلة مرحلة أولى وثانية ابتدائي - جماعي",
-  "arts_grade3_4_group": "فنون التشكيلة مرحلة ثالثة ورابعة ابتدائي - جماعي",
-  "arts_grade5_6_group": "فنون التشكيلة مرحلة خامسة وسادسة ابتدائي - جماعي",
+
+  // المسابقة الإلكترونية
+  "electronic_level1_individual": "المستوى الأول - فردي - إلكترونية",
+  "electronic_level2_individual": "المستوى الثاني - فردي - إلكترونية",
+  "electronic_level1_group": "المستوى الأول - جماعي - إلكترونية",
+  "electronic_level2_group": "المستوى الثاني - جماعي - إلكترونية",
+
+  // الفنون التشكيلية
+  "arts_kindergarten_individual": "مرحلة حضانة - فردي - فنون تشكيلية",
+  "arts_grade1_2_individual": "أولى وثانية ابتدائي - فردي - فنون تشكيلية",
+  "arts_grade3_4_individual": "ثالثة ورابعة ابتدائي - فردي - فنون تشكيلية",
+  "arts_grade5_6_individual": "خامسة وسادسة ابتدائي - فردي - فنون تشكيلية",
+  "arts_kindergarten_group": "مرحلة حضانة - جماعي - فنون تشكيلية",
+  "arts_grade1_2_group": "أولى وثانية ابتدائي - جماعي - فنون تشكيلية",
+  "arts_grade3_4_group": "ثالثة ورابعة ابتدائي - جماعي - فنون تشكيلية",
+  "arts_grade5_6_group": "خامسة وسادسة ابتدائي - جماعي - فنون تشكيلية",
+
+  // الفنون التشكيلية - إبداع حر
   "free_arts_kindergarten_individual": "إبداع حر - حضانة - فردي",
   "free_arts_grade1_2_individual": "إبداع حر - أولى وثانية ابتدائي - فردي",
   "free_arts_grade3_4_individual": "إبداع حر - ثالثة ورابعة ابتدائي - فردي",
@@ -68,26 +187,16 @@ const competitionNamesInArabic = {
   "free_arts_grade1_2_group": "إبداع حر - أولى وثانية ابتدائي - جماعي",
   "free_arts_grade3_4_group": "إبداع حر - ثالثة ورابعة ابتدائي - جماعي",
   "free_arts_grade5_6_group": "إبداع حر - خامسة وسادسة ابتدائي - جماعي",
+
+  // المسابقة الأدبية
   "literary_poetry": "الشعر - فردي",
   "literary_short_story": "القصة القصيرة - فردي",
-  "engineering_programming_mechanics_individual": "برمجة وكهرباء وميكانيكا - فردي",
-  "engineering_programming_mechanics_group": "برمجة وكهرباء وميكانيكا - جماعي",
+
+  // الابتكارات الهندسية
+  "engineering_programming_mechanics_individual": "البرمجة والكهرباء والميكانيكا - فردي",
+  "engineering_programming_mechanics_group": "البرمجة والكهرباء والميكانيكا - جماعي",
   "engineering_architecture_individual": "العمارة - فردي",
   "engineering_architecture_group": "العمارة - جماعي",
-  "football_boys": "كرة القدم - بنين",
-  "football_girls": "كرة القدم - بنات",
-  "volleyball_boys": "الكرة الطائرة - بنين",
-  "volleyball_girls": "الكرة الطائرة - بنات",
-  "table_tennis_boys_individual": "تنس الطاولة - بنين - فردي",
-  "table_tennis_boys_team": "تنس الطاولة - بنين - جماعي",
-  "table_tennis_girls_individual": "تنس الطاولة - بنات - فردي",
-  "table_tennis_girls_team": "تنس الطاولة - بنات - جماعي",
-  "chess_boys_individual": "الشطرنج - بنين - فردي",
-  "chess_boys_team": "الشطرنج - بنين - جماعي",
-  "chess_girls_individual": "الشطرنج - بنات - فردي",
-  "chess_girls_team": "الشطرنج - بنات - جماعي",
-  "running_boys": "الجري - بنين - فردي",
-  "running_girls": "الجري - بنات - فردي"
 };
 
 const churches = [
